cd2db091d959e79ef8bf9b13e3b035cdcac30ad8
Morten Lyng Rosenquist <MLR@delegate.dk>
2021-09-02 16:14:48 +0200

    Lab 1 update

--- 1-python-sockets/client.py
+++ 1-python-sockets/client.py
@@ -1,37 +1,65 @@
 import os
 import sys
+from enum import IntEnum
 
-from enum import Enum
 from socket import *
 
-class MessageType(Enum):
-    String = '1',
-    Binary = '2'
+class MessageType(IntEnum):
+    String = 1,
+    Binary_1B = 2,
+    Binary_2B = 3,
+    Binary_3B = 4,
+    File = 5
+
+def read_file(filename):
+    try:
+        with open('./' + filename, 'r') as f:
+            data = f.read()
+            return data
+    except EnvironmentError as e:
+        print("Error writing file: {}".format(e))
+        return None
 
 while True:
-    messageType: MessageType = input('1 for string, 2 for binary')
+    messageType = MessageType(int(input('1 for string, 2-4 for binary, 5 for file')))
 
-    if(messageType == '1'):
-        type = 1
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(("localhost", 9000))
+    s.send(messageType.to_bytes(length=1, byteorder=sys.byteorder))
 
+    if(messageType == MessageType.String):
         userInput = input("Write what you want to send to the server")
-        s = socket(AF_INET, SOCK_STREAM)
-        s.connect(("localhost", 9000))
-        s.send(type.to_bytes(length=1, byteorder=sys.byteorder))
-
         s.send(bytes(userInput[0: 4096], 'utf-8'))
-        s.close()
 
-    elif(messageType == '2'):
-        userInput = print("Sending random binary to server")
-        type = 2
-
-        randomBytes = bytearray(os.urandom(1000000))
-        s = socket(AF_INET, SOCK_STREAM)
-        s.connect(("localhost", 9000))
-
-        s.send(type.to_bytes(length=1, byteorder=sys.byteorder))
+    elif(messageType == MessageType.Binary_1B):
+        print("Sending random binary_1B to server")
+        randomBytes = bytearray(os.urandom(255))
         s.send(randomBytes)
-        s.close()
+
+    elif (messageType == MessageType.Binary_2B):
+        print("Sending random binary_2B to server")
+        randomBytes = bytearray(os.urandom(64000))
+        s.send(randomBytes)
+
+    elif (messageType == MessageType.Binary_3B):
+        print("Sending random binary_3B to server")
+        randomBytes = bytearray(os.urandom(16000000))
+        s.send(randomBytes)
+    elif (messageType == MessageType.File):
+        fileName = input("Enter filename")
+        fileExists = os.path.isfile(fileName)
+        if(fileExists == True):
+            print('File exists')
+            s.send(bytes(fileName, 'utf-8'))
+
+            response = s.recv(100).decode('utf-8');
+            if(response == "OK"):
+                print("Acknowledge received")
+                fileData = read_file(fileName)
+                s.send(bytes(fileData, 'utf-8'))
+
+
+
+    s.close();
 
 
--- 1-python-sockets/server.py
+++ 1-python-sockets/server.py
@@ -1,5 +1,13 @@
 import _thread, sys, random, string
+from enum import IntEnum
 from socket import *
+
+class MessageType(IntEnum):
+    String = 1,
+    Binary_1B = 2,
+    Binary_2B = 3,
+    Binary_3B = 4,
+    File = 5
 
 def write_file(data, filename=None):
     """
@@ -30,15 +38,34 @@ def write_file(data, filename=None):
 def handleConnection(c: socket, a):
     print('Received connection from {}'.format(a))
     messageTypeBytes = c.recv(1)
-    messageType = int.from_bytes(messageTypeBytes, byteorder=sys.byteorder)
+    messageType = MessageType(int.from_bytes(messageTypeBytes, byteorder=sys.byteorder))
 
-    data = c.recv(1000000)
-    if(messageType == 1):
+    if(messageType == MessageType.String):
+        data = c.recv(1000000)
         print('String data received')
         print('Message: {}'.format(data, 'utf-8'))
-    elif(messageType == 2):
-        print('Binary data received')
-        write_file(data, 'hello.txt')
+    elif(messageType == MessageType.Binary_1B):
+        print('Binary_1B data received')
+        data = c.recv(255)
+        if (len(data) == 255):
+            write_file(data, 'binary_1B.txt')
+    elif (messageType == MessageType.Binary_2B):
+        print('Binary_2B data received')
+        data = c.recv(64000)
+        if (len(data) == 64000):
+            write_file(data, 'binary_2B.txt')
+    elif (messageType == MessageType.Binary_3B):
+        print('Binary_3B data received')
+        data = c.recv(16000000)
+        if(len(data) == 16000000):
+            write_file(data, 'binary_3B.txt')
+    elif (messageType == MessageType.File):
+        print('File is to be received')
+        fileName = c.recv(100).decode('utf-8')
+        c.send(bytes('OK', 'utf-8'))
+        data = c.recv(16000000)
+        write_file(data, "server_"+fileName)
+
     c.close()
 
 s = socket(AF_INET, SOCK_STREAM)
